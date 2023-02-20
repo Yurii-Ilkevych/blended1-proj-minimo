83b89ce4779a7bd031e22901c5df8bb1ba94b57e
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-02-20 16:59:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'src/index.html'),
         restaurant: resolve(__dirname, 'src/restaurant.html'),
-        room: resolve(__dirname, 'src/room.html'),
         hotel: resolve(__dirname, 'src/hotel.html'),
-       
+        room: resolve(__dirname, 'src/room.html'),
       },
     },
     outDir: '../dist',
