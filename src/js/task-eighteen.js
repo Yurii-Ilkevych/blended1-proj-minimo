9b2c65e860b566effdfe3d76d44d1748e44aa6ea
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-02-20 18:37:15 +0200

    added modal window room

--- src/js/task-eighteen.js
+++ src/js/task-eighteen.js
@@ -1,1 +1,16 @@
 // Task 18 - Book Room Modal window
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-room]"),
+      closeModalBtn: document.querySelector("[data-modal-close-room]"),
+      modal: document.querySelector("[data-modal-room]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle("is-hidden-r");
+    }
+  })();
